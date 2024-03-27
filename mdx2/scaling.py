masking bug fix
The intent of the _masked  function is to apply the same mask to many different arrays, so that when compressed they have the same length. In certain circumstances, function is called with a masked array as an argument, causing it to combine the two masks. This was solved using the np.ma.getdata() function, which returns an unmasked array from a regular or masked array argument.

mdx2/scaling.py
@@ -586,7 +586,7 @@ class ScaledData:
         return len(self._batches)
 
     def _masked(self,arr):
-        return np.ma.MaskedArray(data=arr,mask=self.mask,copy=False)
+        return np.ma.MaskedArray(data=np.ma.getdata(arr),mask=self.mask,copy=False)
 
     @property
     def I(self):