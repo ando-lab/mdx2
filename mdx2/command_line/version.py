2ce86268b3e4357ab8b0eb1112db54ea39e05c2b
Steve Meisburger <spm82@cornell.edu>
2024-08-22 15:08:53 -0400

    more info to mdx2.version

--- mdx2/command_line/version.py
+++ mdx2/command_line/version.py
@@ -3,8 +3,10 @@ Print the version number
 """
 
 import argparse
+import sys
+import os
 
-from mdx2 import getVersionNumber
+import mdx2
 
 parser = argparse.ArgumentParser(
     description=__doc__,
@@ -12,7 +14,9 @@ parser = argparse.ArgumentParser(
 
 def run(args=None):
     args = parser.parse_args(args)
-    print("mdx2:",getVersionNumber())
+    print("mdx2:",mdx2.getVersionNumber())
+    print("Python {0.major}.{0.minor}.{0.micro}".format(sys.version_info))
+    print(f"Installed in: {os.path.split(mdx2.__file__)[0]}")
 
 if __name__ == "__main__":
     run()
